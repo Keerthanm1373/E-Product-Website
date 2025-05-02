8204b4ee0ec1c3e982e1c077951e32d131909640
Keerthan <keerthanm1373@gmail.com>
2025-05-02 10:54:09 +0530

    added hash routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Auth/Login";
 import Register from "./Auth/Register";
 import Home from "./Components/Home";
